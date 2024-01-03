d11539f47629c819e601fdb15f05bbad241a92da
NeoCheung <zm07520@163.com>
2024-01-03 13:48:17 +0800

    🎉 init: 初始化代码

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <common-table />
+
+</template>
+
+<script>
+import CommonTable from './components/CommonTable.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CommonTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CommonTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-table :data="tableData" tooltip-effect="light">
+      <el-table-column label="User name" prop="name"></el-table-column>
+      <el-table-column label="Desc." prop="desc"></el-table-column>
+      <el-table-column label="Addresses" show-overflow-tooltip>
+        <template #default="{ row }">
+          {{ row.addresses.join('\n') }}
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script setup>
+const tableData = [
+  {
+    name: 'zhangsan',
+    desc: 'A nice man',
+    addresses: [
+      'Abet ex elit minim ullamco sint eu ut laboris eiusmod.',
+      'Minim fugiat occaecat ipsum culpa laborum aute incididunt id.',
+      'Proident qui minim enim et consectetur eiusmod.',
+      'Est duis enim labore minim.',
+      'Nulla quis eu qui ad voluptate.',
+    ],
+  },
+];
+</script>
+
+<style>
+.is-light {
+  max-width: 330px !important;
+}
+.el-popper {
+  white-space: pre-line !important;
+}
+</style>
